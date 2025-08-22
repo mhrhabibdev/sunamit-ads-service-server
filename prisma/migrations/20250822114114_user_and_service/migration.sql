fa2ad1a6646891079b1473515040cf42fd092bce
MHR HABIB <mhrhabib2004@gmail.com>
2025-08-22 22:30:00 +0600

    feat: add user and ad service models with relationships and order management

--- prisma/migrations/20250822114114_user_and_service/migration.sql
+++ prisma/migrations/20250822114114_user_and_service/migration.sql
@@ -19,5 +19,24 @@ CREATE TABLE "public"."users" (
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."adservices" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT,
+    "category" TEXT NOT NULL,
+    "description" TEXT,
+    "duration" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isPopular" BOOLEAN NOT NULL DEFAULT false,
+    "isNew" BOOLEAN NOT NULL DEFAULT false,
+    "isRecommended" BOOLEAN NOT NULL DEFAULT false,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "adservices_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");

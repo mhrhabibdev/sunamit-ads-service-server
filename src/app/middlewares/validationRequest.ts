6b46b6162a4fb93c84618460c4378ef265873d62
MHR HABIB <mhrhabib2004@gmail.com>
2025-10-08 20:57:33 +0600

    refactor: update validationRequest middleware to use ZodSchema and streamline request body parsing

--- src/app/middlewares/validationRequest.ts
+++ src/app/middlewares/validationRequest.ts
@@ -1,11 +1,10 @@
-import { AnyZodObject } from './../../../node_modules/zod/v3/types.d';
+import { ZodSchema } from "zod";
+import catchAsync from "../utils/catchAsync";
+import { NextFunction, Request, Response } from "express";
 
-import { NextFunction, Request, Response } from 'express';
-import catchAsync from '../utils/catchAsync';
-
-export const validationRequest = (schema: AnyZodObject) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    await schema.parseAsync({ body: req.body });
+export const validationRequest = (schema: ZodSchema) => {
+  return catchAsync(async (req: Request, _res: Response, next: NextFunction) => {
+    await schema.parseAsync(req.body);
     next();
   });
 };

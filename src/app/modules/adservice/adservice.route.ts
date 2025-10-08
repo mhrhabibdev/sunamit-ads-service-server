414fd46c12b1419669838b8c7ee2f6407ad25f3b
MHR HABIB <mhrhabib2004@gmail.com>
2025-10-08 20:57:43 +0600

    refactor: update adservice routes to include validation middleware for create and update operations

--- src/app/modules/adservice/adservice.route.ts
+++ src/app/modules/adservice/adservice.route.ts
@@ -1,14 +1,26 @@
 import { Router } from "express";
 import { AdServiceController } from "./adservice.controller";
-
-
+import { validationRequest } from "../../middlewares/validationRequest";
+import { AdValidation } from "./adservice.validation";
 
 const router = Router();
 
-router.post("/",AdServiceController.createAdService);
+router.post(
+  "/create-adservice",
+  validationRequest(AdValidation.createAdServiceValidation),
+  AdServiceController.createAdService
+);
+
 router.get("/", AdServiceController.getAdAllServices);
+
 router.get("/:id", AdServiceController.getAdServiceById);
-router.patch("/:id", AdServiceController.updateAdService);
+
+router.patch(
+  "/:id",
+  validationRequest(AdValidation.updateAdServiceValidation),
+  AdServiceController.updateAdService
+);
+
 router.delete("/:id", AdServiceController.deleteAdService);
 
 export const AdServiceRoutes = router;
